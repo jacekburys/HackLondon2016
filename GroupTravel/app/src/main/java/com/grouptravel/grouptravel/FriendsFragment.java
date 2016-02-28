818def759b7bb0389095171991df968a9ed7edd6
Jacek Burys <jsb314@ic.ac.uk>
2016-02-28 00:46:52 +0000

    2 tabs work

--- GroupTravel/app/src/main/java/com/grouptravel/grouptravel/FriendsFragment.java
+++ GroupTravel/app/src/main/java/com/grouptravel/grouptravel/FriendsFragment.java
@@ -1,19 +1,151 @@
 package com.grouptravel.grouptravel;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by jacek on 27/02/16.
  */
 public class FriendsFragment extends Fragment {
 
+    MyCustomAdapter dataAdapter = null;
+
+    ArrayList<Friend> friendsList;
+    View rootView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.empty, container, false);
+        rootView = inflater.inflate(R.layout.list, container, false);
+        getFriends();
         return rootView;
+    }
+
+    private void setAdapter(ArrayList<Friend> friendsList) {
+        //create an ArrayAdaptar from the String Array
+        this.friendsList = friendsList;
+        dataAdapter = new MyCustomAdapter(getContext(),
+                R.layout.friend_item, friendsList);
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+    }
+
+    private void getFriends(){
+
+        final ArrayList<Friend> friends = new ArrayList<>();
+
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/friends",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        try{
+                            JSONArray arr = response.getJSONObject().getJSONArray("data");
+                            for(int i=0; i<arr.length(); i++) {
+                                JSONObject obj = arr.getJSONObject(i);
+                                Log.d("friend", obj.toString());
+                                String name = obj.get("name").toString();
+                                String id = obj.get("id").toString();
+                                Log.d("friend added", name + " - - " + id);
+                                Friend friend = new Friend(name, id);
+                                friends.add(friend);
+                            }
+
+                            setAdapter(friends);
+
+                        } catch(JSONException e){
+
+                        }
+                    }
+                }
+        ).executeAsync();
+
+
+    }
+
+
+
+    private class MyCustomAdapter extends ArrayAdapter<Friend> {
+
+        private ArrayList<Friend> friendsList;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               ArrayList<Friend> friendsList) {
+            super(context, textViewResourceId, friendsList);
+            this.friendsList = new ArrayList<Friend>();
+            this.friendsList.addAll(friendsList);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater)getContext().getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.friend_item, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener( new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v ;
+                        Friend friend = (Friend) cb.getTag();
+                        Toast.makeText(getContext().getApplicationContext(),
+                                "Clicked on Checkbox: " + cb.getText() +
+                                        " is " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        friend.setSelected(cb.isChecked());
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Friend country = friendsList.get(position);
+            holder.code.setText(" (" +  country.getId() + ")");
+            holder.name.setText(country.getName());
+            holder.name.setChecked(country.isSelected());
+            holder.name.setTag(country);
+
+            return convertView;
+
+        }
+
     }
 }
---
+++ GroupTravel/app/src/main/java/com/grouptravel/grouptravel/LoginActivity.java
@@ -0,0 +1,162 @@
+package com.grouptravel.grouptravel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookActivity;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends Activity {
+
+    //private TextView textView;
+    private LoginButton loginButton;
+    //private Button showButton;
+    private CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        /*
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.grouptravel.grouptravel",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+        */
+
+
+        setContentView(R.layout.login_layout);
+        //textView = (TextView)findViewById(R.id.textView);
+        loginButton = (LoginButton)findViewById(R.id.loginButton);
+        //showButton = (Button)findViewById(R.id.button);
+        loginButton.setReadPermissions("user_friends");
+
+        callbackManager = CallbackManager.Factory.create();
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Debug", "success");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        goToMain();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("Debug", "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.d("Debug", "error");
+            }
+        });
+
+        /*
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFriends();
+            }
+        });
+        */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    private void goToMain() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
+
+}
